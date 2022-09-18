50e3bb07a1f6df89e1c7a05b9bb0d96547b2671b
ClydeTime <42362589+ClydeTime@users.noreply.github.com>
2022-09-19 02:08:08 +0800

    Update wps_test.js

--- Script/wps_test.js
+++ Script/wps_test.js
@@ -2,14 +2,11 @@
 
 脚本功能：WPS解锁稻壳会员
 软件版本：11.26.0
-下载地址：http://t.cn/A6ZD2hl3
-脚本作者：Hausd0rff
 更新时间：2022-07-05
-脚本发布：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_777
-使用声明：⚠️此脚本仅供学习与交流，
-        请勿转载与贩卖！⚠️⚠️⚠️
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
 *******************************
+
 [rewrite_local]
 # > WPS解锁稻壳会员
 ^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/ClydeTime/Quantumult/main/Script/wps_test.js
@@ -25,11 +22,15 @@ var objc = JSON.parse(body);
 
 objc.vips = [
     {
-      "memberid" : 12,
-      "expire_time" : 32495476149,
-      "name" : "稻壳会员",
-      "has_ad" : 0,
-      "enabled" : null
+     name: "超级会员",
+     has_ad: 0,
+     memberid: 40,
+     expire_time: 4102415999,
+     enabled: [
+      { memberid: 40, name: "超级会员", expire_time: 4102415999 },
+      { memberid: 20, name: "WPS会员", expire_time: 4102415999 },
+      { memberid: 12, name: "稻壳会员", expire_time: 4102415999 }
+    ]
     }
 ];
 
