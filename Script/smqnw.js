bfb582f0e17ab6ee153c20085e18af083c2e53e2
ClydeTime <42362589+ClydeTime@users.noreply.github.com>
2022-09-19 08:16:11 +0800

    Update smqnw.js

--- Script/smqnw.js
+++ Script/smqnw.js
@@ -18,7 +18,7 @@ hostname = ap*.intsig.net
 let body = JSON[parse]($response[body]);
 body[data]["psnl_vip_property"] = {
   renew_method: appstore,
-  initial_tm: 1662040830,
+  initial_tm: 1614867690,
   svip: 1,
   auto_renewal: !![],
   ms_first_pay: 0,
@@ -26,11 +26,11 @@ body[data]["psnl_vip_property"] = {
   group2_paid: 0,
   inherited_flag: 0,
   nxt_renew_tm: "4417948799",
-  level_info: { level: 1, days: 1, end_days: 30 },
+  level_info: { level: 2, days: 1, end_days: 30 },
   group1_paid: 1,
   ys_first_pay: 0,
   renew_type: year,
-  expiry: 4102415999,
+  expiry: 8487890487,
   grade: 2,
   last_payment_method: appstore,
   product_id: com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low
