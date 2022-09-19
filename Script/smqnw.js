daa5c51807a8ff17299f8650c09f12a16de433d3
ClydeTime <42362589+ClydeTime@users.noreply.github.com>
2022-09-19 10:45:05 +0800

    Update smqnw.js

--- Script/smqnw.js
+++ Script/smqnw.js
@@ -16,28 +16,25 @@ hostname = ap*.intsig.net
 
 **************************/
 
-let body = JSON[parse]($response[body]);
+let body = JSON.parse($response.body);
 body[data]["psnl_vip_property"] = {
-      "product_id" : "com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low",
-      "initial_tm" : "1614867690",
-      "svip" : 1,
-      "auto_renewal" : true,
-      "ms_first_pay" : 0,
-      "pending" : 0,
-      "group2_paid" : 0,
-      "inherited_flag" : 0,
-      "nxt_renew_tm" : "1614867690",
-      "level_info" : {
-        "level" : 1,
-        "days" : 1,
-        "end_days" : 30
-      },
-      "group1_paid" : 1,
-      "ys_first_pay" : 0,
-      "renew_type" : "year",
-      "expiry" : 8487890487,
-      "grade" : 2,
-      "renew_method" : "appstore",
-      "last_payment_method" : "appstore"
+  renew_method: "appstore",
+  initial_tm: "1614867690",
+  svip: 1,
+  auto_renewal: true,
+  ms_first_pay: 0,
+  pending: 0,
+  group2_paid: 0,
+  inherited_flag: 0,
+  nxt_renew_tm: "9915126887",
+  level_info: { level: 1, days: 1, end_days: 30 },
+  group1_paid: 1,
+  ys_first_pay: 0,
+  renew_type: "year",
+  expiry: 8487890487,
+  grade: 2,
+  last_payment_method: "appstore",
+  product_id: "com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low"
 };
-$done({ body: JSON[stringify](body) });
+
+$done({ body: JSON.stringify(body) });
