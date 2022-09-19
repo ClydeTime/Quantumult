7cfa3c50d28079a86b2ca5eea7eb22bc434ff03d
ClydeTime <42362589+ClydeTime@users.noreply.github.com>
2022-09-19 11:15:34 +0800

    Update smqnw.js

--- Script/smqnw.js
+++ Script/smqnw.js
@@ -17,7 +17,7 @@ hostname = ap*.intsig.net
 **************************/
 
 let body = JSON.parse($response.body);
-body[data][psnl_vip_property] = {
+body.data[psnl_vip_property] = {
   renew_method: "appstore",
   initial_tm: "1614867690",
   svip: 1,
@@ -26,7 +26,7 @@ body[data][psnl_vip_property] = {
   pending: 0,
   group2_paid: 0,
   inherited_flag: 0,
-  nxt_renew_tm: "1614867690",
+  nxt_renew_tm: "9915126887",
   level_info: { level: 1, days: 1, end_days: 30 },
   group1_paid: 1,
   ys_first_pay: 0,
