67fcc3975c5c5de4925817e24ac414102885a8a9
ClydeTime <42362589+ClydeTime@users.noreply.github.com>
2022-09-20 15:16:09 +0800

    Update wpsDocker.js

--- Script/wpsDocker.js
+++ Script/wpsDocker.js
@@ -14,17 +14,17 @@ hostname = *.docer.wps.cn, vipapi.wps.cn
 
 *******************************/
 
-var string = [
+var modifyHeaders = [
   "headers",
   "Cookie",
   "wps_sid=V02SX4VqzgWOwZnb8SIefL9aw_S756800a221e7c004f984b69;wpsua=V1BTVUEvMS4wKGlvcy1vZmZpY2U6MTEuMjYuMDExODY3MDtpUGhvbmU6MTQuMjtVRElELURCRTI2NTM0LTY5QUEtNEZFMi1CRTRCLTQ1QTdGQzM1NTg1NjphVkJvYjI1bClhcHBsZS9pUGhvbmUxMiwx;SEC_SESSION=be338ae5-8ee7-4340-9c02-c9c99b5177b1;Sec_User_id=57345519",
   "User-Agent",
   "Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 WpsiOS/11.26.0",
   "IP",
-  "223.5.5.5"
+  "223.5.5.5",
 ];
-var clydeTime = request[string[0]];
-clydeTime[string[1]] = string[2];
-clydeTime[string[3]] = string[4];
-clydeTime[string[5]] = string[6];
-$done({ headers: clydeTime });
+var hausd0rff = $request[modifyHeaders[0]];
+hausd0rff[modifyHeaders[1]] = modifyHeaders[2];
+hausd0rff[modifyHeaders[3]] = modifyHeaders[4];
+hausd0rff[modifyHeaders[5]] = modifyHeaders[6];
+$done({ headers: hausd0rff });
