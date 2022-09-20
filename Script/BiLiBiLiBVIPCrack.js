96e80f85d1fc88f4aebc6d9f8925dd959c2d9bd8
ClydeTime <42362589+ClydeTime@users.noreply.github.com>
2022-09-20 20:20:13 +0800

    Update BiLiBiLiBVIPCrack.js

--- Script/BiLiBiLiBVIPCrack.js
+++ Script/BiLiBiLiBVIPCrack.js
@@ -3,11 +3,7 @@
  *
 脚本功能：哔哩哔哩解锁大会员
 软件版本：6.58.0
-下载地址：http://t.cn/AiOqwnn1
-脚本作者：Hausd0rff
-更新时间：2022-01-18
-脚本发布：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_777
+更新时间：2022-09-20
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
@@ -24,11 +20,11 @@ hostname = *.bili*.*, 124.239.240.*, 101.89.57.66, 218.94.210.66, 240e:b1:9801:2
 */
 
 var modifiedHeaders = $request['headers'];
-modifiedHeaders['Cookie'] = '_uuid=4A3CE9C2-1078-A30A-19EA-C22B81664B6B63925infoc; buvid3=760FCB2E-CE9A-4A71-AACC-155BB626B652148806infoc; Buvid=Y24D822FB1AA2A704A7E98D6D7DBADFAC7AB; sid=je9bz5k2; SESSDATA=fddb001c%2C1658059735%2C843ca711; bili_jct=c669d4cf4eb0fbbb7d2f7db9b493856f; DedeUserID=1054839627; DedeUserID__ckMd5=79cbf5b08990c190';
-modifiedHeaders['Authorization'] = 'identify_v1 ff14d9063a2ebd1f0eac2d5fb475a511';
-modifiedHeaders['User-Agent'] = 'bili-universal/65600100 os/ios model/iPhone 11 mobi_app/iphone osVer/14.2 network/1';
+modifiedHeaders['Cookie'] = 'Buvid=ZC4147CFB1A9154740EFAB429F1FE215383E; DedeUserID=18866638; DedeUserID__ckMd5=068cecaa89d60c4c; SESSDATA=8611887c%2C1679225351%2Ccf5fea91; bili_jct=03dc098ee4b9c1a73b63e704cdee569f; sid=6b4nlvk9';
+modifiedHeaders['Authorization'] = 'identify_v1 b01dfd8d053ab64187aa3697410a2591';
+modifiedHeaders['User-Agent'] = 'bili-universal/69000100 os/ios model/iPhone 12 Pro Max mobi_app/iphone osVer/16.0 network/2';
 modifiedHeaders['x-bili-locale-bin'] = 'Eg4KAnpoEgRIYW5zGgJDTg==';
-modifiedHeaders['x-bili-device-bin'] = 'CAEQ5PSjHxokWTI0RDgyMkZCMUFBMkE3MDRBN0U5OEQ2RDdEQkFERkFDN0FCIgZpcGhvbmUqA2lvczIFcGhvbmU6BWFwcGxlQgVBcHBsZUoJaVBob25lIDExUgQxNC4yagY2LjU2LjByQERFNDUwRkM1NjA5MUVCNUE5NEUyQkYwRDUwRDcxOERFMjAyMTAyMTYxNzQzMTM5ODExMjlCNUY0QjJCMzM2Njh43u+Lm+Yv';
-modifiedHeaders['x-bili-metadata-bin'] = 'CiBmZjE0ZDkwNjNhMmViZDFmMGVhYzJkNWZiNDc1YTUxMRIGaXBob25lGgVwaG9uZSDk9KMfKgVhcHBsZTIkWTI0RDgyMkZCMUFBMkE3MDRBN0U5OEQ2RDdEQkFERkFDN0FCOgNpb3M=';
-modifiedHeaders['x-bili-fawkes-req-bin'] = 'CgZpcGhvbmUSBHByb2QaCDRkNTRhZDc5';
+modifiedHeaders['x-bili-device-bin'] = 'CAEQpLfzIBokWkM0MTQ3Q0ZCMUE5MTU0NzQwRUZBQjQyOUYxRkUyMTUzODNFIgZpcGhvbmUqA2lvczIFcGhvbmU6BWFwcGxlQgVBcHBsZUoRaVBob25lIDEyIFBybyBNYXhSBDE2LjBqBjYuOTAuMHJAOTQwNkY4MkQyQkUwQjc0QjcwQjgwOENBNUNBM0RFNjgyMDE5MDkxNDE0MzczMEZFRDgzMjQ2NzkzRUE2RUM3M3i/o7KD4C4=';
+modifiedHeaders['x-bili-metadata-bin'] = 'CiBiMDFkZmQ4ZDA1M2FiNjQxODdhYTM2OTc0MTBhMjU5MRIGaXBob25lGgVwaG9uZSCkt/MgKgVhcHBsZTIkWkM0MTQ3Q0ZCMUE5MTU0NzQwRUZBQjQyOUYxRkUyMTUzODNFOgNpb3M=';
+modifiedHeaders['x-bili-fawkes-req-bin'] = 'CgZpcGhvbmUSBHByb2QaEGZmZmZmZmZmMDE3OTg0MDM=';
 $done({ 'headers': modifiedHeaders });
