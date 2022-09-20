0a1e60984cf9de139b9b310888330636434e73db
ClydeTime <42362589+ClydeTime@users.noreply.github.com>
2022-09-20 14:16:34 +0800

    Update wpsDocker.js

--- Script/wpsDocker.js
+++ Script/wpsDocker.js
@@ -1,7 +1,7 @@
 /******************************
 脚本功能：WPS解锁稻壳会员
-软件版本：11.26.0
-更新时间：2022-07-05
+软件版本：11.29.2
+更新时间：2022-09-20
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 
@@ -14,19 +14,13 @@ hostname = *.docer.wps.cn, vipapi.wps.cn
 
 *******************************/
 
-var string = [
-  "headers",
-  "Cookie",
-  "wps_sid=V02SX4VqzgWOwZnb8SIefL9aw_S756800a221e7c004f984b69;wpsua=V1BTVUEvMS4wKGlvcy1vZmZpY2U6MTEuMjYuMDExODY3MDtpUGhvbmU6MTQuMjtVRElELURCRTI2NTM0LTY5QUEtNEZFMi1CRTRCLTQ1QTdGQzM1NTg1NjphVkJvYjI1bClhcHBsZS9pUGhvbmUxMiwx;SEC_SESSION=be338ae5-8ee7-4340-9c02-c9c99b5177b1;Sec_User_id=57345519",
-  "User-Agent",
-  "Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 WpsiOS/11.26.0",
-  "IP",
+var vip_user = [
+  "wps_sid=V02S23O9y69VRf31ohDvD_IKBM9-8Os00a10ef0f002bf5687d;wpsua=V1BTVUEvMS4wKGlvcy1vZmZpY2U6MTEuMjkuMjEyMjA1NztpUGhvbmU6MTYuMDtVRElELTMwQTYxNjU5LUY3QTItNDRBMy05NTBGLTQ5MzMyNjgwRDY0ODphVkJvYjI1bClhcHBsZS9pUGhvbmUxMyw0;csrf=ECAiAJJDQdZPMXGzk22m2MGmjYeZTDDd;ulocale=zh-CN;uzone=CN-HN",
+  "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148WpsiOS m-meeting-sdk/3.0.0",
   "223.5.5.5"
 ];
-var clydeTime = $request[string[0]];
-clydeTime[string[1]] = string[2];
-clydeTime[string[3]] = string[4];
-clydeTime[string[5]] = string[6];
-$done({
-  headers: clydeTime
-});
+var modifiedHeaders = $request["headers"];
+modifiedHeaders["Cookie"] = vip_user[0];
+modifiedHeaders["User-Agent"] = vip_user[1];
+modifiedHeaders["IP"] = vip_user[2];
+$done({ headers: modifiedHeaders });
