07d4f4d93aa5805d519a5b02b1b8019cc5223220
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-09-21 13:03:16 +0800

    Update bili5.js

--- Script/BiliBili/bili5.js
+++ Script/BiliBili/bili5.js
@@ -13,7 +13,7 @@ body.data = {
 	vip_overdue_time: 0,
 	vip_type: 2,
 	tv: "超级大会员：有效期至2099-12-31",
-	face: "http://i1.hdslb.com/bfs/face/6e49b6f3fdcacecb511be4c06467792fc7bbf236.jpg",
+	face: body.data.face.toString(),
 	vip_status: 1,
 	user_name: body.data.user_name.toString(),
 	vip: "大会员：有效期至2099-12-31",
