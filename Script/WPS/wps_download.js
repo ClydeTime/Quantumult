f71437da627f4ebc4c0c38e5dab6997148f63203
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-09-22 16:09:49 +0800

    Update wps_download.js

--- Script/WPS/wps_download.js
+++ Script/WPS/wps_download.js
@@ -15,8 +15,10 @@ hostname = *.docer.wps.cn, vipapi.wps.cn
 var vip_user = [
   "wps_sid=V02SaEzMoENCwCQNeTIcUVvchy9ICc400a321c51002bf5687d",
   "Mozilla/5.0 (iPad; CPU OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+  "223.5.5.5"
 ];
 var modifiedHeaders = $request["headers"];
 modifiedHeaders["Cookie"] = vip_user[0];
 modifiedHeaders["User-Agent"] = vip_user[1];
+modifiedHeaders["IP"] = vip_user[2];
 $done({ headers: modifiedHeaders });
