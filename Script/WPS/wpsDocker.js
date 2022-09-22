8a5bd556b72d2b7190836e478ba4315d72580c2f
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-09-22 17:17:00 +0800

    Update wpsDocker.js

--- Script/WPS/wpsDocker.js
+++ Script/WPS/wpsDocker.js
@@ -15,8 +15,8 @@ hostname = *.docer.wps.cn, vipapi.wps.cn
 *******************************/
 
 var vip_user = [
-  "wps_sid=V02SUNUA3PALA04bF7a2bmCe09igluw00ab6b66e002bf5687d; wpsua=V1BTVUEvMS4wKGlvcy1vZmZpY2U6MTEuMjkuMjEyMjA1NztpUGhvbmU6MTYuMDtVRElELTMwQTYxNjU5LUY3QTItNDRBMy05NTBGLTQ5MzMyNjgwRDY0ODphVkJvYjI1bClhcHBsZS9pUGhvbmUxMyw0",
-  "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 WpsiOS/11.29.2",
+  "wps_sid=V02SaEzMoENCwCQNeTIcUVvchy9ICc400a321c51002bf5687d; wpsua=V1BTVUEvMS4wKGlvcy1vZmZpY2U6MTEuMjkuMjEyMjA1NztpUGFkOjE1LjU7VURJRC1DMTMwRUNBMi02Q0Q5LTQxOEMtOTc0MC02MjU5MkM0MkQ0NDg6NWJTVTVZcWI1WS1MNTVxRUlHbFFZV1E9KWFwcGxlL2lQYWQxMSwx",
+  "Mozilla/5.0 (iPad; CPU OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 WpsiOS/11.29.2",
 ];
 var modifiedHeaders = $request["headers"];
 modifiedHeaders["Cookie"] = vip_user[0];
