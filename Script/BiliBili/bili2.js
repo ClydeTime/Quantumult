36c31314da17ce455ce2dd6fbbf70ba2ee9a4ba7
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-09-22 21:30:02 +0800

    Update bili2.js

--- Script/BiliBili/bili2.js
+++ Script/BiliBili/bili2.js
@@ -21,32 +21,5 @@ body.data.card.level_info.current_exp = 39900;
 body.data.card.level_info.next_exp = "--";
 body.data.card.space_tag_bottom[0].title = "IP属地：火星";
 
-body.data.card.nameplate = {
-        level: "稀有勋章",
-        condition: "单个自制视频总播放数>=100万",
-        nid: 1,
-        name: "黄金殿堂",
-        image: "http://i1.hdslb.com/bfs/face/82896ff40fcb4e7c7259cb98056975830cb55695.png",
-        image_small: "http://i1.hdslb.com/bfs/face/627e342851dfda6fe7380c2fa0cbd7fae2e61533.png"
-};
-body.data.card.vip = {
-	vipStatusWarn: "",
-	vipType: 2,
-	dueRemark: "",
-	vipDueDate: 4102329600000,
-	accessStatus: 0,
-	vipStatus: 1,
-	themeType: 0,
-	label: {
-		bg_color: "#FB7299",
-		bg_style: 1,
-		text: "年度大会员",
-		border_color: "",
-		path: "",
-		image: "https://i0.hdslb.com/bfs/vip/8d4f8bfc713826a5412a0a27eaaac4d6b9ede1d9.png",
-		label_theme: "annual_vip",
-		text_color: "#FFFFFF"
-	}
-};
-body.data.vip_space_label.show_expire = false;
+
 $done({ body: JSON.stringify(body) });
