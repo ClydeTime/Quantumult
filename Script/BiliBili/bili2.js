f50ad6f93ad1dff5cc9279f281b015e27176a722
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-09-26 21:07:30 +0800

    Update bili2.js

--- Script/BiliBili/bili2.js
+++ Script/BiliBili/bili2.js
@@ -25,15 +25,34 @@ if (!body.data) {
     $done({});
 }
 
-if("vip_space_label" in body.data){  
-  body.data.card.fans = 9999999;
-  body.data.card.level_info.current_level = 6;
-  body.data.card.level_info.identity = 2;
-  body.data.card.level_info.current_min = 28800;
-  body.data.card.level_info.current_exp = 39900;
-  body.data.card.level_info.next_exp = "--";
-  body.data.card.likes.like_num = 9999999;
-  body.data.card.attention = 0;
+if("vip_space_label" in body.data){
+	body.data.card.fans = 9999999;
+	body.data.card.level_info.current_level = 6;
+	body.data.card.level_info.identity = 2;
+	body.data.card.level_info.current_min = 28800;
+	body.data.card.level_info.current_exp = 39900;
+	body.data.card.level_info.next_exp = "--";
+	body.data.card.likes.like_num = 9999999;
+	body.data.card.attention = 0;
+	body.data.card.vip = {
+		vipStatusWarn: "",
+		vipType: 2,
+		dueRemark: "",
+		vipDueDate: 4102329600000,
+		accessStatus: 0,
+		vipStatus: 1,
+		themeType: 0,
+		label: {
+			bg_color: "#FB7299",
+			bg_style: 1,
+			text: "年度大会员",
+			border_color: "",
+			path: "",
+			image: "https://i0.hdslb.com/bfs/vip/8d7e624d13d3e134251e4174a7318c19a8edbd71.png",
+			label_theme: "annual_vip",
+			text_color: "#FFFFFF"
+		}
+	};
 
   if(body.data.card.name == "这就进入我知识盲区了" || body.data.card.name == "令狐少侠121"){
     body.data.images.imgUrl = "http://i0.hdslb.com/bfs/garb/item/43057105228c287930db1427c21650148c4fd6b1.jpg";
@@ -123,25 +142,6 @@ if("vip_space_label" in body.data){
         break;
       } 
     }
-  } 
-  body.data.card.vip = {
-    vipStatusWarn: "",
-    vipType: 2,
-    dueRemark: "",
-    vipDueDate: 4102329600000,
-    accessStatus: 0,
-    vipStatus: 1,
-    themeType: 0,
-    label: {
-      bg_color: "#FB7299",
-      bg_style: 1,
-      text: "年度大会员",
-      border_color: "",
-      path: "",
-      image: "https://i0.hdslb.com/bfs/vip/8d7e624d13d3e134251e4174a7318c19a8edbd71.png",
-      label_theme: "annual_vip",
-      text_color: "#FFFFFF"
-    }
-  };
+  }
 }
 $done({ body: JSON.stringify(body) });
