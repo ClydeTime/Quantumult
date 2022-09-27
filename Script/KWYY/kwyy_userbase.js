5b37ef9694c9e6173d268ec11cf4fc63ea696d4f
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-09-27 14:40:26 +0800

    Update kwyy_userbase.js

--- Script/KWYY/kwyy_userbase.js
+++ Script/KWYY/kwyy_userbase.js
@@ -25,15 +25,15 @@ if (!body.data) {
 }
 
 body.data.ui = {
-    isNewUser: body.data.ui.isNewUser.toString(),
+    isNewUser: body.data.ui.isNewUser,
     vipExpire: "4102329600000",
     vipOverSeasExpire: "0",
     isYearUser: "2",
     vip3Expire: "0",
     biedAlbum: "1",
     vipmAutoPayUser: "2",
-    time: body.data.ui.time.toString(),
-    vipmExpire: "4102329600000,
+    time: body.data.ui.time,
+    vipmExpire: "4102329600000",
     luxAutoPayUser: "2",
     vipLuxuryExpire: "4102329600000",
     svipExpire: "4102329600000",
@@ -42,5 +42,22 @@ body.data.ui = {
     experienceExpire: "0"
 }
 
+body.data.vipui = {
+    isComplainUser: body.data.vipui.isComplainUser,
+    luxuryIcon: "https://image.kuwo.cn/fe/2fae68ff-de2d-4473-bf28-8efc29e44968vip.png",
+    province: body.data.vipui.province,
+    vipmAutoPayUser: "2",
+    vipmExpire: "4102329600000",
+    isQQKugou: body.data.vipui.isQQKugou,
+    luxAutoPayUser: "2",
+    vipTag: "VIP6",
+    vipLuxuryExpire: "4102329600000",
+    vipmIcon: "https://image.kuwo.cn/fe/34ad47f8-da7f-43e4-abdc-e6c995666368yyb.png",
+    city: body.data.vipui.citytoString(),
+    svipIcon: "https://image.kuwo.cn/fe/f2d09ac0-b959-404f-86fa-dc65c715c0e96.png",
+    svipExpire: "4102329600000",
+    svipAutoPayUser: "2",
+    experienceExpire: 0
+}
 
 $done({ body: JSON.stringify(body) });
