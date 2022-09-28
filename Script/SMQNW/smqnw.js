2c2575df599f8f1014fd353f106728d1b431c386
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-09-28 11:06:55 +0800

    Update smqnw.js

--- Script/SMQNW/smqnw.js
+++ Script/SMQNW/smqnw.js
@@ -18,43 +18,4 @@ hostname = ap*.intsig.net
 
 **************************/
 
-var body = JSON.parse($response.body);
-body["data"] = {
-    fax_balance : "1",
-    used_points : "0",
-    cert_mode_balance : 0,
-    login_ocr_balance : "10",
-    points : "99999",
-    psnl_vip_property : {
-      product_id : "com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low",
-      initial_tm : "1662040800",
-      svip : 1,
-      auto_renewal : true,
-      ms_first_pay : 0,
-      pending : 0,
-      group2_paid : 0,
-      inherited_flag : 0,
-      nxt_renew_tm : "9915126887",
-      level_info : {
-        level : 1,
-        days : 1,
-        end_days : 30
-      },
-      group1_paid : 1,
-      ys_first_pay : 0,
-      renew_type : "year",
-      expiry : 4102415999,
-      grade : 2,
-      renew_method : "appstore",
-      last_payment_method : "appstore"
-    },
-    pdfword_balance : "1",
-    bookmode_balance : 999,
-    immt_expy_points : "0",
-    ocr_balance : 1000,
-    no_login_ocr_balance : "2",
-    server_time : body.data.server_time.toString(),
-    CamScanner_RoadMap : "0"
-};
-
-$done({ body: JSON.stringify(body) });
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('m D=\'R.E.S\',T=[\'‮D\'],w=[D,\'1e\',\'1f=\',\'1g\',\'1h/1i==\',\'1j\',\'1k==\',\'1l\',\'1m+1n==\',\'1o==\',\'1p+1q\',\'1r.E.1s==\'];o(r(h,i,j){r U(a,b,c,d,e,f){b=b>>1t,e=\'1u\';m g=\'1v\',I=\'1w\',f=\'‮\';o(b<a){1x(--a){d=h[g]();o(b===a&&f===\'‮\'&&f[\'y\']===t){b=d,c=h[e+\'p\']()}V o(b&&c[\'W\'](/[1y=]/g,\'\')===b){h[I](d)}}h[I](h[g]())}z 1z};z U(++i,j)>>i^j}(w,X,1A),w){T=w[\'y\']^X};r k(g,h){g=~~\'1B\'[\'1C\'](g[\'Y\'](t));m i=w[g];o(k[\'Z\']===F){(r(){m d=G 11!==\'F\'?11:G 1D===\'12\'&&G 1E===\'r\'&&G 13===\'12\'?13:1F;m e=\'1G+/=\';d[\'J\']||(d[\'J\']=r(a){m b=K(a)[\'W\'](/=+$/,\'\');A(m c=l,H,u,14=l,L=\'\';u=b[\'1H\'](14++);~u&&(H=c%15?H*1I+u:u,c++%15)?L+=K[\'16\'](1J&H>>(-M*c&1K)):l){u=e[\'1L\'](u)}z L})}());r 17(a,b){m c=[],n=l,B,N=\'\',O=\'\';a=J(a);A(m d=l,18=a[\'y\'];d<18;d++){O+=\'%\'+(\'1M\'+a[\'P\'](d)[\'1N\'](1O))[\'Y\'](-M)}a=1P(O);A(m e=l;e<v;e++){c[e]=e}A(e=l;e<v;e++){n=(n+c[e]+b[\'P\'](e%b[\'y\']))%v;B=c[e];c[e]=c[n];c[n]=B}e=l;n=l;A(m f=l;f<a[\'y\'];f++){e=(e+t)%v;n=(n+c[e])%v;B=c[e];c[e]=c[n];c[n]=B;N+=K[\'16\'](a[\'P\'](f)^c[(c[e]+c[n])%v])}z N}k[\'19\']=17;k[\'Q\']={};k[\'Z\']=!![]}m j=k[\'Q\'][g];o(j===F){o(k[\'1a\']===F){k[\'1a\']=!![]}i=k[\'19\'](i,h);k[\'Q\'][g]=i}V{i=j}z i};m C=1b[\'1Q\']($1R[k(\'‮0\',\'1S\')]);C[k(\'‮1\',\'1T\')]={\'1U\':\'1\',\'1V\':\'0\',\'1W\':l,\'1X\':\'10\',\'1Y\':k(\'‮2\',\'%1Z!\'),\'20\':{\'21\':\'E.22.23.24.25.26.1c.27\',\'28\':k(\'‫3\',\'29\'),\'1c\':t,\'2a\':!![],\'2b\':l,\'2c\':l,\'2d\':l,\'2e\':l,\'2f\':k(\'‫4\',\'2g%\'),\'2h\':{\'2i\':t,\'2j\':t,\'2k\':2l},\'2m\':t,\'2n\':l,\'2o\':k(\'‫5\',\'%2p\'),\'2q\':2r,\'2s\':M,\'2t\':\'1d\',\'2u\':\'1d\'},\'2v\':\'1\',\'2w\':2x,\'2y\':\'0\',\'2z\':2A,\'2B\':\'2\',\'2C\':C[k(\'‮6\',\'%2D\')][k(\'‮7\',\')8)s\')][k(\'‮8\',\'x%2E\')](),\'2F\':\'0\'};$2G({\'C\':1b[k(\'‮9\',\'h&(q\')](C)});D=\'R.E.S\';',62,167,'||||||||||||||||||||_0x279a|0x0|var|_0xb0f567|if|||function||0x1|_0x2a97ab|0x100|_0x58d5||length|return|for|_0x1ff4ee|body|_0xod0|com|undefined|typeof|_0x21ed6e|_0x41c3a9|atob|String|_0x2f9f47|0x2|_0x486ff5|_0x25d566|charCodeAt|RQDBJh|jsjiami|v6|_0xod0_|_0x43b736|else|replace|0x1a4|slice|fYWKhS||window|object|global|_0x1289e1|0x4|fromCharCode|_0x30d792|_0x55fd3e|uiFARX|RQEryH|JSON|svip|appstore|wqPDtkwl|w5HCmMKtw6U|w7oGU30g|dx|CqkHCt8KdwpdnwqDDtQ|KcO7w6HDksOMByfCmlZT|w7DCgsOYOg|w5krKMKn|GV5GRm5|w7XDjcKZw7rCsQ|YhMua2JSw6XDuQ|wpTDvzE|PGdcYMKT|ZUqUjsjiamli|vp6VlIbKwRC|0x8|po|shift|push|while|ZUqUlpVlIbKwRC|0x10597d|0x1a400|0x|concat|process|require|this|ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789|charAt|0x40|0xff|0x6|indexOf|00|toString|0x10|decodeURIComponent|parse|response|xWwk|UGhO|fax_balance|used_points|cert_mode_balance|login_ocr_balance|points|mj|psnl_vip_property|product_id|intsig|camscanner|premiums|oneyear|autorenewable|low|initial_tm|ZCgT|auto_renewal|ms_first_pay|pending|group2_paid|inherited_flag|nxt_renew_tm|E9O|level_info|level|days|end_days|0x1e|group1_paid|ys_first_pay|renew_type|gK9|expiry|0xf485e67f|grade|renew_method|last_payment_method|pdfword_balance|bookmode_balance|0x3e7|immt_expy_points|ocr_balance|0x3e8|no_login_ocr_balance|server_time|Z6y|jb|CamScanner_RoadMap|done'.split('|'),0,{}))
