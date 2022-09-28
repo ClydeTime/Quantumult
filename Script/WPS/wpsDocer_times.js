5615286226c3446f55f90078f8a24013fb8fc96c
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-09-28 11:12:38 +0800

    Update wpsDocer_times.js

--- Script/WPS/wpsDocer_times.js
+++ Script/WPS/wpsDocer_times.js
@@ -13,24 +13,4 @@
 hostname = *.docer.wps.cn
 **************************/
 
-const url = $request.url;
-const method = $request.method;
-if (!$response.body) {
-    // 有undefined的情况
-    console.log("body is undefined!");
-    $done({});
-}
-let body = JSON.parse($response.body);
-const buy = "/buy";
-const userinfo = "/userinfo";
-
-if (url["indexOf"](buy) != -1 && method == "GET") {
-	body.data.is_docer_vip = 1;
-	body.data.free_times = 300;
-}
-if (url["indexOf"](userinfo) != -1 && method == "GET") {
-	body.data.total = 300;
-	body.data.remain = 300;
-}
-
-$done({ body: JSON.stringify(body) });
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('l B=\'T.U.G\',W=[\'‮B\'],v=[B,\'1n==\',\'1o=\',\'1p==\',\'1q\',\'1r=\',\'1s==\',\'1t/1u==\',\'1v=\',\'1w==\',\'1x=\',\'1y=\',\'1z\',\'1A==\',\'1B=\',\'1C=\',\'1D=\',\'1E.1F.G==\'];n(r(h,i,j){r X(a,b,c,d,e,f){b=b>>1G,e=\'1H\';l g=\'1I\',H=\'1J\',f=\'‮\';n(b<a){1K(--a){d=h[g]();n(b===a&&f===\'‮\'&&f[\'w\']===t){b=d,c=h[e+\'p\']()}Y n(b&&c[\'Z\'](/[1L=]/g,\'\')===b){h[H](d)}}h[H](h[g]())}x 1M};x X(++i,j)>>i^j}(v,10,1N),v){W=v[\'w\']^10};r k(g,h){g=~~\'1O\'[\'1P\'](g[\'11\'](t));l i=v[g];n(k[\'12\']===C){(r(){l d=D 13!==\'C\'?13:D 1Q===\'14\'&&D 1R===\'r\'&&D 15===\'14\'?15:1S;l e=\'1T+/=\';d[\'I\']||(d[\'I\']=r(a){l b=J(a)[\'Z\'](/=+$/,\'\');y(l c=o,E,s,16=o,K=\'\';s=b[\'1U\'](16++);~s&&(E=c%17?E*1V+s:s,c++%17)?K+=J[\'18\'](1W&E>>(-19*c&1X)):o){s=e[\'1a\'](s)}x K})}());r 1b(a,b){l c=[],m=o,A,L=\'\',M=\'\';a=I(a);y(l d=o,1c=a[\'w\'];d<1c;d++){M+=\'%\'+(\'1Y\'+a[\'N\'](d)[\'1Z\'](20))[\'11\'](-19)}a=21(M);y(l e=o;e<u;e++){c[e]=e}y(e=o;e<u;e++){m=(m+c[e]+b[\'N\'](e%b[\'w\']))%u;A=c[e];c[e]=c[m];c[m]=A}e=o;m=o;y(l f=o;f<a[\'w\'];f++){e=(e+t)%u;m=(m+c[e])%u;A=c[e];c[e]=c[m];c[m]=A;L+=J[\'18\'](a[\'N\'](f)^c[(c[e]+c[m])%u])}x L}k[\'1d\']=1b;k[\'O\']={};k[\'12\']=!![]}l j=k[\'O\'][g];n(j===C){n(k[\'1e\']===C){k[\'1e\']=!![]}i=k[\'1d\'](i,h);k[\'O\'][g]=i}Y{i=j}x i};F P=$1f[k(\'‫0\',\'1g\')];F Q=$1f[k(\'‮1\',\'22\')];n(!$1h[\'q\']){23[\'24\'](\'q\\25\\26!\');$1i({})}27 q=1j[k(\'‮2\',\'V@1k\')]($1h[k(\'‫3\',\'28\')]);F R=\'/R\';F 1l=k(\'‫4\',\'29\');n(P[k(\'‮5\',\'2a\')](R)!=-t&&Q==k(\'‫6\',\'1g\')){q[k(\'‮7\',\'2b\')][k(\'‫8\',\'2c\')]=t;q[k(\'‫9\',\'@2d\')][k(\'‫a\',\'V@1k\')]=S}n(P[\'1a\'](1l)!=-t&&Q==\'2e\'){q[k(\'‫b\',\')!2f\')][k(\'‮c\',\'2g\')]=S;q[k(\'‮d\',\'z%2h\')][k(\'‮e\',\'^1m\')]=S}$1i({\'q\':1j[k(\'‮f\',\'^1m\')](q)});B=\'T.U.G\';',62,142,'||||||||||||||||||||_0x4ebf|var|_0x36add0|if|0x0||body|function|_0x21cc59|0x1|0x100|_0x4e7e|length|return|for||_0x455efd|_0xodN|undefined|typeof|_0x345d3d|const|v6|_0x38e105|atob|String|_0x162929|_0x342132|_0x1133f0|charCodeAt|UPcIaW|url|method|buy|0x12c|jsjiami|com||_0xodN_|_0x5e7751|else|replace|0x12e|slice|wctDdn|window|object|global|_0x3a9bd7|0x4|fromCharCode|0x2|indexOf|_0x849664|_0xfe1f64|HJbpvh|UzrSvU|request|Ggcm|response|done|JSON|gE|userinfo|wL0|w65lVsKRGg|GDfDjWM|GFrDhMOJwpJbwqc9PA|YsK9S8OoSlIL|wofDiwo|bQNQeA|wqcgw7vDuMKbw5|CisOge8Khw4DCqQ|w7bCr8Kyw5Q|w7h2QcKHIA5IGnfCvw|wr9kKhs|FcO3TcOnw4A|woVDKsKg|wr9jZsKLwqPDgw|wr5yecKDwqTDinDCpWw|wrXDvDI|E8KffsKIZyk|yNbjAGsjtiCawKWmqi|dcKkFoRm|0x8|po|shift|push|while|yNbAGtCwKWqdKkFR|0x105983|0x12e00|0x|concat|process|require|this|ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789|charAt|0x40|0xff|0x6|00|toString|0x10|decodeURIComponent|Gacv|console|log|x20is|x20undefined|let|bkHt|bkho|3xOY|cH9i|IV6K|A0Q|GET|TZ|69rG|ZO'.split('|'),0,{}))
