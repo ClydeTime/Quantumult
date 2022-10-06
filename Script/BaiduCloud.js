557e5ae7bb5a89a26a85bdf7ee137d3491b2f2b1
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-10-06 13:45:35 +0800

    Update BaiduCloud.js

--- Script/BaiduCloud.js
+++ Script/BaiduCloud.js
@@ -17,15 +17,15 @@ let body = JSON.parse($response.body);
 body.product_infos = [{
 	product_id: "5210897792128633390",
 	end_time: 4102415999,
-    buy_time: "1577808000",
-    cluster: "offlinedl",
-    status: "1",
-    start_time: 1577808000,
-    function_num: 2,
-    buy_description: "离线下载套餐(永久)",
-    product_description: "离线下载套餐(永久)",
-    detail_cluster: "offlinedl",
-    product_name: "offlinedl_permanent"
+	buy_time: "1577808000",
+	cluster: "offlinedl",
+	status: "1",
+	start_time: 1577808000,
+	function_num: 2,
+	buy_description: "离线下载套餐(永久)",
+	product_description: "离线下载套餐(永久)",
+	detail_cluster: "offlinedl",
+	product_name: "offlinedl_permanent"
 }, 
 {
 	product_id: "14040884078253912436",
@@ -33,19 +33,19 @@ body.product_infos = [{
 	buy_time: 0,
 	cluster: "vip",
 	status: 1,
-    start_time: 1577808000,
-    function_num: 4,
-    buy_description: "高级用户套餐",
-    product_description: "超级会员"
-    detail_cluster: "svip",
-    product_name: "svip2_nd"
+	start_time: 1577808000,
+	function_num: 4,
+	buy_description: "高级用户套餐",
+	product_description: "超级会员"
+	detail_cluster: "svip",
+	product_name: "svip2_nd"
 }];
 body.level_info = {
-    history_value: 58000,
-    current_level: 8,
-    last_manual_collection_time: 0,
-    current_value: 58000,
-    history_level: 8
+	history_value: 58000,
+	current_level: 8,
+	last_manual_collection_time: 0,
+	current_value: 58000,
+	history_level: 8
 };
 body.user_tag = "{\"has_buy_record\":0,\"has_buy_vip_svip_record\":0,\"last_buy_record_creat_time\":0,\"is_vip\":0,\"is_svip\":1,\"last_vip_type\":0,\"last_vip_svip_end_time\":1640966399,\"is_svip_sign\":1,\"notice_user_type\":1,\"notice_user_status\":0,\"is_first_act\":0}";
 
