08e2ce346053d7876b444bd3d54c0251cbb79911
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-10-14 12:29:18 +0800

    Update 52pojie.js

--- Script/Task/52pojie.js
+++ Script/Task/52pojie.js
@@ -20,11 +20,11 @@ QuantumultX 远程脚本配置:
 
 [task_local]
 # 吾爱签到
-0 9 * * * https://raw.githubusercontent.com/NobyDa/Script/master/52pojie-DailyBonus/52pojie.js
+5 9 * * * https://raw.githubusercontent.com/ClydeTime/Quantumult/main/Script/Task/52pojie.js
 
 [rewrite_local]
 # 获取Cookie
-https:\/\/www\.52pojie\.cn\/home\.php\? url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/52pojie-DailyBonus/52pojie.js
+https:\/\/www\.52pojie\.cn\/home\.php\? url script-request-header https://raw.githubusercontent.com/ClydeTime/Quantumult/main/Script/Task/52pojie.js
 
 [mitm] 
 hostname= www.52pojie.cn
@@ -69,7 +69,7 @@ if ($.env.isRequest) {
 
 function GetCookie() {
   const TM = $.read("TIME");
-  const CK = $request.headers['Cookie'] || $request.headers['cookie'];
+  const CK = $request.headers['Cookie'];
   if (CK && CK.includes('_auth=')) {
     $.write(CK, "COOKIE");
     if (!TM || TM && (Date.now() - TM) / 1000 >= 21600) {
