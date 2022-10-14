4316ef62d5bbcc3a21abd135a799d224a42f6f23
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-10-14 18:49:32 +0800

    Update Notability.js

--- Script/Notability.js
+++ Script/Notability.js
@@ -1,7 +1,7 @@
 /******************************
 
 脚本功能：Notability +解锁订阅
-更新时间：2022-9-8
+更新时间：2022-10-14
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 
@@ -18,9 +18,9 @@ hostname = notability.com
 
 *******************************/
 
-var obj = JSON["parse"]($response["body"]);
+var body = JSON["parse"]($response["body"]);
 var modifiedStatus = "HTTP/1.1 200 OK";
-obj = {
+body = {
   data: {
     processAppleReceipt: {
       __typename: "SubscriptionResult",
@@ -28,9 +28,9 @@ obj = {
       subscription: {
         __typename: "AppStoreSubscription",
         status: "active",
-        originalPurchaseDate: "2020-09-28T09:09:09.000Z",
-        originalTransactionId: "100109876543210",
-        expirationDate: "2999-09-28T09:09:09.000Z",
+        originalPurchaseDate: "2022-01-01T00:00:00.000Z",
+        originalTransactionId: "12345987654321",
+        expirationDate: "2099-12-31T11:59:59.000Z",
         productId: "com.yqc.premium_subscription",
         tier: "premium",
         refundedDate: null,
@@ -40,4 +40,4 @@ obj = {
     }
   }
 };
-$done({ status: modifiedStatus, body: JSON["stringify"](obj) });
+$done({ status: modifiedStatus, body: JSON["stringify"](body) });
