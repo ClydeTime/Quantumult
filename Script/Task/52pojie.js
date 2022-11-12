f55833c472c5439e1b8ab29b6e7c2d0ada5fd7db
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-11-12 23:19:39 +0800

    Update 52pojie.js

--- Script/Task/52pojie.js
+++ Script/Task/52pojie.js
@@ -21,11 +21,11 @@ QuantumultX 远程脚本配置:
 
 [task_local]
 # 吾爱签到
-5 9 * * * https://raw.githubusercontent.com/NobyDa/Script/master/52pojie-DailyBonus/52pojie.js
+5 9 * * * https://raw.githubusercontent.com/ClydeTime/Quantumult/main/Script/Task/52pojie.js
 
 [rewrite_local]
 # 获取Cookie
-https:\/\/www\.52pojie\.cn\/home\.php\? url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/52pojie-DailyBonus/52pojie.js
+https:\/\/www\.52pojie\.cn\/home\.php\? url script-request-header https://raw.githubusercontent.com/ClydeTime/Quantumult/main/Script/Task/52pojie.js
 
 [mitm] 
 hostname= www.52pojie.cn
