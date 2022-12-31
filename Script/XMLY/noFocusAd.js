45c0c223e21fe64e69b14fb6d0ff754c2c0a356a
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2022-12-31 09:02:59 +0800

    更新 noFocusAd.js

--- Script/XMLY/noFocusAd.js
+++ Script/XMLY/noFocusAd.js
@@ -15,13 +15,7 @@ hostname= *.ximalaya.com
 
 let body = JSON.parse($response.body);
 
-var length = body.header[0].item.list[0].data.length;
-let arr = [];
-for(let i = 0; i < length; i++) {
-	if(body.header[0].item.list[0].data[i].isAd == false) {
-		arr.push(body.header[0].item.list[0].data[i]);
-	}
-}
-body.header[0].item.list[0].data = arr;
-
+body.replace(/(\{"isAd.*?\})/g,x=>{
+   return x.match('isAd":true') ? '' : x ;
+})
 $done({ body: JSON.stringify(body) });
