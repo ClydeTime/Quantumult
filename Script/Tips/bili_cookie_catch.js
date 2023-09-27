d8faf16b645bc057da4cfda49ee04b24fdd4244f
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2023-09-27 09:59:52 +0800

    Update bili_cookie_catch.js

--- Script/Tips/bili_cookie_catch.js
+++ Script/Tips/bili_cookie_catch.js
@@ -2,7 +2,7 @@
 脚本名称：获取B站共享Cookie
 脚本作者：MartinsKing
 使用方法：打开B站客户端自动弹出
-更新时间：2022-10-31
+更新时间：2023-09-27
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
@@ -15,30 +15,15 @@
 hostname= app.bilibili.com
 */
 
-const cookie2object = (cookie) => {
-  var obj = {};
-  var arr = cookie.split("; ");
-  arr.forEach(function (val) {
-    var brr = val.split("=");
-    obj[brr[0]] = brr[1];
-  });
-  return obj;
-};
 const config = {
-  cookie: {},
   headers: {}
 };
 
 config.headers = $request.headers;
-config.cookie = cookie2object(config.headers.Cookie);
 var nessary_headers = {};
-nessary_headers.Cookie = `DedeUserID=${config.cookie.DedeUserID}; DedeUserID__ckMd5=${config.cookie.DedeUserID__ckMd5}; SESSDATA=${config.cookie.SESSDATA}; bili_jct=${config.cookie.bili_jct}; sid=${config.cookie.sid}`;
+nessary_headers.buvid = config.headers.buvid;
 nessary_headers.Authorization = config.headers.Authorization;
 nessary_headers['User-Agent'] = config['headers']['User-Agent'];
-nessary_headers['x-bili-locale-bin'] = config['headers']['x-bili-locale-bin'];
-nessary_headers['x-bili-device-bin'] = config['headers']['x-bili-device-bin'];
-nessary_headers['x-bili-metadata-bin'] = config['headers']['x-bili-metadata-bin'];
-nessary_headers['x-bili-fawkes-req-bin'] = config['headers']['x-bili-fawkes-req-bin'];
 console.log(JSON.stringify(nessary_headers));
 $notify("BiliBili-cookie获取", "获取成功", JSON.stringify(nessary_headers));
 $done({});
